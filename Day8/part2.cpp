725c4f0cecc009a7e28c090190814fe27f1e8721
Chiliveri Praveen <52811013+praveenchiliveri6@users.noreply.github.com>
2024-12-08 04:49:12 -0700

    Update part2.cpp

--- Day8/part2.cpp
+++ Day8/part2.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 string input;
 vector<string> matrix;
-ll n,m,antennasCount;
+ll n,m,antiNodesAtAntennasCount;
 set<pair<ll,ll>> antiNodeLocations;
 unordered_map<char,vector<pair<ll,ll>>> charToIndexMap;
 
 pair<ll,ll> findSlope(pair<ll,ll> p1,pair<ll,ll> p2){
+    // slope = (y2-y1)/(x2-x1)
     ll x1=p1.first,y1=p1.second;
     ll x2=p2.first,y2=p2.second;
     ll num=y2-y1;
@@ -25,13 +26,13 @@ void findAntiNodesWithSlope(pair<ll,ll> cord1, pair<ll,ll> cord2, pair<ll,ll> sl
     // y=y1+slope(x-x1)
     ll x1=cord1.first,y1=cord1.second;
     ll x2=cord2.first,y2=cord2.second;
-    ll mx=slope.first,my=slope.second;
+    ll my=slope.first,mx=slope.second;
     ll totalCount=0;
     for(ll x=1;x<=n;x++){
-        ll y=mx*(x-x1);
-        if(my==0 or y%my!=0)
+        ll y=my*(x-x1);
+        if(mx==0 or y%mx!=0)
             continue;
-        y/=my;
+        y/=mx;
         y+=y1;
         if(y>=1 and y<=m and x!=x2 and y!=y2 and x!=x1 and y!=y1 and matrix[x-1][y-1]=='.'){
             antiNodeLocations.insert({x,y});
@@ -42,10 +43,10 @@ void findAntiNodesWithSlope(pair<ll,ll> cord1, pair<ll,ll> cord2, pair<ll,ll> sl
     swap(y1,y2);
     
     for(ll x=1;x<=n;x++){
-        ll y=mx*(x-x1);
-        if(my==0 or y%my!=0)
+        ll y=my*(x-x1);
+        if(mx==0 or y%mx!=0)
             continue;
-        y/=my;
+        y/=mx;
         y+=y1;
         if(y>=1 and y<=m and x!=x2 and y!=y2 and x!=x1 and y!=y1 and matrix[x-1][y-1]=='.'){
             antiNodeLocations.insert({x,y});
@@ -63,13 +64,12 @@ void calculateNumberOfAntiNodes(){
                 charToIndexMap[matrix[row][col]].push_back({row+1,col+1});
             }
     
-    ll totalAntiNodesCnt=0;
     for(auto entry:charToIndexMap){
         vector<pair<ll,ll>> indexVector=entry.second;
-        ll sz=indexVector.size();
-        antennasCount+=(sz>1?sz:0);
-        for(ll index1=0;index1<sz-1;index1++)
-            for(ll index2=1;index2<sz;index2++){
+        ll totalIndexes=indexVector.size();
+        antiNodesAtAntennasCount+=(totalIndexes>1?totalIndexes:0);
+        for(ll index1=0;index1<totalIndexes-1;index1++)
+            for(ll index2=index1+1;index2<totalIndexes;index2++){
                 pair<ll,ll> slope=findSlope(indexVector[index1],indexVector[index2]);
                 findAntiNodesWithSlope(indexVector[index1],indexVector[index2],slope);
             }
@@ -92,6 +92,6 @@ int main() {
     
     getInput();
     calculateNumberOfAntiNodes();
-    cout<<antiNodeLocations.size()+antennasCount;
+    cout<<antiNodeLocations.size()+antiNodesAtAntennasCount;
     return 0;
 }
